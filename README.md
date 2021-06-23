48d143bd45551e88a95451f50c3bd09ca56af57a
Gil Rtr <glrtr2003@gmail.com>
2021-06-23 19:18:07 +0300

    Made publishable (added files like README)

---
+++ README.md
@@ -0,0 +1,11 @@
+# logic-circus
+
+Logic Circus is a logic circut simulator written in Rust
+
+It is nowhere near finished and I'd like to one day even add a GUI
+
+You can build circuits by composing other circuits together (with
+`Component`) or by fully implementing their logic (with the `GateLike`
+trait)
+
+License: WTFPL
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+#![warn(clippy::cargo_common_metadata)]
 #![allow(unused_unsafe)]
+
+//! Logic Circus is a logic circut simulator written in Rust
+//!
+//! It is nowhere near finished and I'd like to one day even add a GUI
+//! You can build circuits by composing other circuits together (with
+//! `Component`) or by fully implementing their logic (with the `GateLike`
+//! trait)
 
 use std::convert::Infallible;
 
